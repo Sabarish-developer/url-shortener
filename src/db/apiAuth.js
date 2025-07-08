755ca89fff672b3da8bd2bcb82ca6359a95141d9
Sabarish-developer <sabarish.cspro@gmail.com>
2025-07-08 12:47:48 +0530

    add context api

--- src/db/apiAuth.js
+++ src/db/apiAuth.js
@@ -8,3 +8,13 @@ export const login = async ({email, password}) => {
 
     return data;
 }
+
+export const getCurrentUser = async() => {
+
+    const {data, error} = await supabase.auth.getSession();
+
+    if(!data.session) return null;    // User is not logged in
+    if(error) throw new Error(error.message);
+
+    return data.session?.user;        //If data.session exists return data.session.user
+}
