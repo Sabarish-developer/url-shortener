d426b260c92ee03816792403e31c46c75ce8bd05
Sabarish-developer <sabarish.cspro@gmail.com>
2025-07-09 21:57:51 +0530

    add api for urls

---
+++ src/db/apiUrls.js
@@ -0,0 +1,8 @@
+import supabase from "./supabase";
+
+export const getUrls = async(user_id) => {
+
+    const {data, error} = await supabase.from("urls").select("*").eq("user_id", user_id);
+    if(error) throw new Error("Unable to fetch urls");
+    return data;
+}
