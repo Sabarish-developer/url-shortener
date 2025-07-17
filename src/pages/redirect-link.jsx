4f1a87892067c6aa06e8d09616b39a8afda7ed83
Sabarish-developer <sabarish.cspro@gmail.com>
2025-07-17 21:17:41 +0530

    add redirect-link page

--- src/pages/redirect-link.jsx
+++ src/pages/redirect-link.jsx
@@ -1,8 +1,39 @@
+import { getLongUrl } from "@/db/apiUrls";
+import { storeClicks } from "@/db/apiClicks";
+import { useFetch } from "@/hooks/useFetch";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { BarLoader } from "react-spinners";
+
 export const RedirectLink = () => {
 
-    return (
-        <div>
-            Redirect link
-        </div>
-    )
+    const {id} = useParams();
+
+    const {loading, data, fn: fnGelLongUrl} = useFetch(getLongUrl, id);
+    const {loading: loadingClicks, data: clickData, fn: fnStoreClicks} = useFetch(storeClicks, {
+        id: data?.id,
+        originalUrl: data?.original_url
+    });
+
+    useEffect(()=>{
+        fnGelLongUrl();
+    }, []);
+
+    useEffect(()=>{
+        if(!loading && data){
+            fnStoreClicks();
+        }
+    }, [loading]);
+
+    if(loading || loadingClicks){
+        return (
+            <div>
+                <BarLoader width={"100%"} color="#ffffff" />
+                <br />
+                Redirecting....
+            </div>
+        )
+    }
+
+    return null;
 }
