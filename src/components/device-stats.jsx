1ad42aa38b7534d48440eacadddd47388b72c8c9
Sabarish-developer <sabarish.cspro@gmail.com>
2025-07-18 19:18:57 +0530

    add device stats component

--- src/components/device-stats.jsx
+++ src/components/device-stats.jsx
@@ -1,8 +1,36 @@
+import { Pie, PieChart, ResponsiveContainer, Cell } from 'recharts';
 
 export const DeviceStats = ({stats}) => {
+
+    const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+    const deviceCount = stats.reduce((acc, item) => {
+        if(acc[item.device])
+            acc[item.device] += 1;
+        else
+            acc[item.device] = 1;
+        return acc;
+    }, {});
+
+    const devices = Object.entries(deviceCount).map(([device, count]) => ({
+        device,
+        count
+    }));
+
     return (
-        <div>
-            Device stats
-        </div>
-    )
+        <div className='w-full h-[300px]'>
+            <ResponsiveContainer width="100%" height="100%">
+            <PieChart>
+                <Pie data={devices} 
+                dataKey="count" 
+                labelLine={false} 
+                fill="#8884d8" 
+                label = {({device, percent}) => (`${device}: ${(percent*100).toFixed(0)}%`)}
+                >
+                    {devices.map((_, index) => <Cell key={index} fill={colors[index%colors.length]} />)}
+                </Pie>
+            </PieChart>
+            </ResponsiveContainer>
+        </div>  
+    );
 }
